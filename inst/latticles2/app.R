53ff38e3be8673dc0ef4347156415cd4e1ca5629
Anderson R Silva <47083501+arsilva87@users.noreply.github.com>
2020-02-26 10:24:14 -0300

    Add files via upload

--- inst/latticles2/app.R
+++ inst/latticles2/app.R
@@ -9,56 +9,6 @@ check.packages <- function(pkg){
 packages <- c("xml2", "shiny")
 check.packages(packages)
 
-# --------------------------------------------------
-# latticles em lote
-
-latticles2 <- function(direc, anos)
-{
-   if(is.null(direc)) return()
-   if(is.null(anos)) return()
-	
-   # arquivos zip no diretorio
-   files <- list.files(direc)
-   n <- length(files)
-
-   # loop
-   xmls <- quanti <- list()
-   for(i in 1:n) {
-      xmls[[i]] <- read_xml(paste(direc, "\\", files[i], sep = ""))
-      quanti[[i]] <- artigosLattes(XML = xmls[[i]], anos = anos)
-   }
-
-   # artigos pub
-   spam <- do.call("rbind", lapply(quanti, "[[", "metricas"))
-
-   # n revistas em que se publicou
-   #nrev <- sapply(sapply(lapply(lapply(quanti, "[[", "planilha"), "[[", "ISSN"), unique), length)
-
-   # trabalhos e resumos em eventos 
-   spamT <- do.call("rbind", lapply(quanti, "[[", "trabalhos"))
-
-   # livros e capitulos
-   spamL <- do.call("rbind", lapply(quanti, "[[", "livros"))
-   spamC <- do.call("rbind", lapply(quanti, "[[", "capitulos"))
-
-   # orientacoes
-   spamO <- do.call("rbind", lapply(quanti, "[[", "orientacoes"))
-
-   # softwares e patentes
-   spamS <- do.call("rbind", lapply(quanti, "[[", "software"))
-   spamP <- do.call("rbind", lapply(quanti, "[[", "patentes"))
-
-   # output
-   nomes <- sapply(quanti, "[[", "nome")
-   out <- cbind(artigos = spam, trabalhos = spamT,
-      livros = spamL, capitulos = spamC, orien = spamO, 
-      soft = spamS, pat = spamP)
-   out2 <- data.frame("Nome                                                             " = c(nomes, "TOTAL"), 
-      Data = c(sapply(quanti, "[[", "atual"), " "), 
-      rbind(out, TOTAL = colSums(out)))
-   rownames(out2) <- NULL
-   invisible(out2)
-}
 
 # ----------------------------------------------------------
 # function artigosLattes
@@ -70,7 +20,7 @@ artigosLattes <- function(XML, anos)
    if(is.null(anos)) return()
 
    nome <- attr(as_list(xml_children(XML)[[1]]), "NOME-COMPLETO")
-   atuali <- attr(as_list(XML)$"CURRICULO-VITAE", "DATA-ATUALIZACAO")
+   atuali <- attr(as_list(xml_parent(XML))$"CURRICULO-VITAE", "DATA-ATUALIZACAO")
    datat <- strsplit(atuali, "")[[1]]
    datat. <- paste0(datat[1], datat[2], "-", datat[3], datat[4], "-", 
       datat[5], datat[6], datat[7], datat[8])
@@ -79,34 +29,27 @@ artigosLattes <- function(XML, anos)
    qualis$ISSN. <- sapply(strsplit(qualis$ISSN, "-"), paste, collapse = "")
 
    # artigos publicados ------------------------------------------------------
-   prod <- try(xml_children(XML)[[grep("ARTIGOS-PUBLICADOS", xml_children(XML))]], silent = TRUE)
+   prod <- try(xml_children(XML)[[grep("PRODUCAO-BIBLIOGRAFICA", xml_children(XML))]], silent = TRUE)
    artpub <- try(as_list(xml_children(prod))[[grep("ARTIGOS-PUBLICADOS", xml_children(prod))]], silent = TRUE)
 
    if (class(artpub) != "try-error" & !is.null(artpub)) {
       # loop
       n <- length(artpub)
-      artigodat <- data.frame(Autor1 = NA, Ano = NA, Titulo = NA, Periodico = NA, ISSN = NA,
-         Qualis = NA, Vol = NA, pInicial = NA, pFinal = NA)
+      artigodat <- data.frame(Ano = NA, Periodico = NA, ISSN = NA, Qualis = NA)
       i = 1
       repeat {
          art <- artpub[[i]]
          artigodat[i, ] <- 
-            c(strsplit(attr(art[["AUTORES"]], "NOME-PARA-CITACAO"), ";")[[1]][1],
-            attr(art[["DADOS-BASICOS-DO-ARTIGO"]], "ANO-DO-ARTIGO"),
-            attr(art[["DADOS-BASICOS-DO-ARTIGO"]], "TITULO-DO-ARTIGO"),
+            c(attr(art[["DADOS-BASICOS-DO-ARTIGO"]], "ANO-DO-ARTIGO"),
             attr(art[["DETALHAMENTO-DO-ARTIGO"]], "TITULO-DO-PERIODICO-OU-REVISTA"),
             attr(art[["DETALHAMENTO-DO-ARTIGO"]], "ISSN"),
-            qualis[grep(attr(art[["DETALHAMENTO-DO-ARTIGO"]], "ISSN"), qualis$ISSN.)[1], "Estrato"],
-            attr(art[["DETALHAMENTO-DO-ARTIGO"]], "VOLUME"),
-            attr(art[["DETALHAMENTO-DO-ARTIGO"]], "PAGINA-INICIAL"),
-            attr(art[["DETALHAMENTO-DO-ARTIGO"]], "PAGINA-FINAL"))
+            qualis[grep(attr(art[["DETALHAMENTO-DO-ARTIGO"]], "ISSN"), qualis$ISSN.)[1], "Estrato"])
          i = i + 1
          if(i > n) break()
       }
       artigodat$Qualis[is.na(artigodat$Qualis)] = "C"
    } else { 
-      artigodat = data.frame(Autor1 = NA, Ano = NA, Titulo = NA, Periodico = NA, ISSN = NA,
-         Qualis = NA, Vol = NA, pInicial = NA, pFinal = NA)
+      artigodat = data.frame(Ano = NA, Periodico = NA, ISSN = NA, Qualis = NA)
    }
    artigodat$Qualis <- factor(artigodat$Qualis, 
       levels = c("A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C", "NP"))
@@ -114,33 +57,14 @@ artigosLattes <- function(XML, anos)
    # trabalhos em eventos ----------------------------------------------------
    trab <- try(as_list(xml_children(prod))[[grep("TRABALHOS-EM-EVENTOS", xml_children(prod))]], silent = TRUE)
    if (class(trab) != "try-error" & !is.null(trab)) {
-      # loop
-      n3 <- length(trab)
-      trabdat <- data.frame(Autor1 = NA, Ano = NA, Titulo = NA, Natureza = NA, Evento = NA, 
-         Local = NA, ISBN = NA, Vol = NA, pInicial = NA, pFinal = NA)
-      i = 1
-      repeat {
-         a <- trab[[i]]
-         trabdat[i, ] <- 
-            c(strsplit(attr(a[["AUTORES"]], "NOME-PARA-CITACAO"), ";")[[1]][1],
-            attr(a[["DETALHAMENTO-DO-TRABALHO"]], "ANO-DE-REALIZACAO"),
-            attr(a[["DADOS-BASICOS-DO-TRABALHO"]], "TITULO-DO-TRABALHO"),
-            attr(a[["DADOS-BASICOS-DO-TRABALHO"]], "NATUREZA"),
-            attr(a[["DETALHAMENTO-DO-TRABALHO"]], "NOME-DO-EVENTO"),
-            attr(a[["DETALHAMENTO-DO-TRABALHO"]], "CIDADE-DO-EVENTO"),
-            attr(a[["DETALHAMENTO-DO-TRABALHO"]], "ISBN"),
-            attr(a[["DETALHAMENTO-DO-TRABALHO"]], "VOLUME"),
-            attr(a[["DETALHAMENTO-DO-TRABALHO"]], "PAGINA-INICIAL"),
-            attr(a[["DETALHAMENTO-DO-TRABALHO"]], "PAGINA-FINAL"))
-         i = i + 1
-         if(i > n3) break()
-      }
+      trabdat <- data.frame(Ano = as.numeric(sapply(lapply(trab, "[[", "DETALHAMENTO-DO-TRABALHO"), attr, "ANO-DE-REALIZACAO")), 
+         Natureza = sapply(lapply(trab, "[[", "DADOS-BASICOS-DO-TRABALHO"), attr, "NATUREZA"))
    } else { 
-      trabdat = data.frame(Autor1 = NA, Ano = NA, Titulo = NA, Natureza = NA, Evento = NA, 
-         Local = NA, ISBN = NA, Vol = NA, pInicial = NA, pFinal = NA)   
+      trabdat = data.frame(Ano = NA, Natureza = NA)   
    }
    trabdat$Natureza <- factor(trabdat$Natureza, 
-	levels = c("COMPLETO", "RESUMO", "RESUMO_EXPANDIDO"))
+      levels = c("COMPLETO", "RESUMO", "RESUMO_EXPANDIDO"),
+	labels = c("Completo", "Res_simples", "Res_expandido"))
    
    # livros ------------------------------------------------------------------
    licap <- try(as_list(xml_children(prod))[[grep("LIVROS-E-CAPITULOS", xml_children(prod))]], silent = TRUE)
@@ -148,26 +72,20 @@ artigosLattes <- function(XML, anos)
    if (class(livro) != "try-error" & !is.null(livro)) {
       # loop
       n4 <- length(livro)
-      livrodat <- data.frame(Autor1 = NA, Ano = NA, Titulo = NA, Ed = NA,
-         Local = NA, Editora = NA, ISBN = NA, Pags = NA)
+      livrodat <- data.frame(Ano = NA, Titulo = NA, Editora = NA, ISBN = NA)
       i = 1
       repeat {
          a <- livro[[i]]
          livrodat[i, ] <- 
-            c(strsplit(attr(a[["AUTORES"]], "NOME-PARA-CITACAO"), ";")[[1]][1],
-            attr(a[["DADOS-BASICOS-DO-LIVRO"]], "ANO"),
+            c(attr(a[["DADOS-BASICOS-DO-LIVRO"]], "ANO"),
             attr(a[["DADOS-BASICOS-DO-LIVRO"]], "TITULO-DO-LIVRO"),
-            attr(a[["DETALHAMENTO-DO-LIVRO"]], "NUMERO-DA-EDICAO-REVISAO"),
-            attr(a[["DETALHAMENTO-DO-LIVRO"]], "CIDADE-DA-EDITORA"),
             attr(a[["DETALHAMENTO-DO-LIVRO"]], "NOME-DA-EDITORA"),
-            attr(a[["DETALHAMENTO-DO-LIVRO"]], "ISBN"),
-            attr(a[["DETALHAMENTO-DO-LIVRO"]], "NUMERO-DE-PAGINAS"))
+            attr(a[["DETALHAMENTO-DO-LIVRO"]], "ISBN"))
          i = i + 1
          if(i > n4) break()
       }  
    } else { 
-      livrodat <- data.frame(Autor1 = NA, Ano = NA, Titulo = NA, Ed = NA,
-         Local = NA, Editora = NA, ISBN = NA, Pags = NA)
+      livrodat <- data.frame(Ano = NA, Titulo = NA, Editora = NA, ISBN = NA)
    }
 
    # capitulos ---------------------------------------------------------------
@@ -175,52 +93,40 @@ artigosLattes <- function(XML, anos)
    if (class(cap) != "try-error" & !is.null(cap)) {
       # loop
       n4. <- length(cap)
-      capdat <- data.frame(Autor1 = NA, Ano = NA, Titulo = NA, Livro = NA, Ed = NA,
-         Local = NA, Editora = NA, ISBN = NA, pInicial = NA, pFinal = NA)
+      capdat <- data.frame(Ano = NA, Titulo = NA, Livro = NA, Editora = NA, ISBN = NA)
       i = 1
       repeat {
          a <- cap[[i]]
          capdat[i, ] <- 
-            c(strsplit(attr(a[["AUTORES"]], "NOME-PARA-CITACAO"), ";")[[1]][1],
-            attr(a[["DADOS-BASICOS-DO-CAPITULO"]], "ANO"),
+            c(attr(a[["DADOS-BASICOS-DO-CAPITULO"]], "ANO"),
             attr(a[["DADOS-BASICOS-DO-CAPITULO"]], "TITULO-DO-CAPITULO-DO-LIVRO"),
             attr(a[["DETALHAMENTO-DO-CAPITULO"]], "TITULO-DO-LIVRO"),
-            attr(a[["DETALHAMENTO-DO-CAPITULO"]], "NUMERO-DA-EDICAO-REVISAO"),
-            attr(a[["DETALHAMENTO-DO-CAPITULO"]], "CIDADE-DA-EDITORA"),
             attr(a[["DETALHAMENTO-DO-CAPITULO"]], "NOME-DA-EDITORA"),
-            attr(a[["DETALHAMENTO-DO-CAPITULO"]], "ISBN"),
-            attr(a[["DETALHAMENTO-DO-CAPITULO"]], "PAGINA-INICIAL"),
-            attr(a[["DETALHAMENTO-DO-CAPITULO"]], "PAGINA-FINAL"))
+            attr(a[["DETALHAMENTO-DO-CAPITULO"]], "ISBN"))
          i = i + 1
          if(i > n4.) break()
       }
    } else { 
-      capdat <- data.frame(Autor1 = NA, Ano = NA, Titulo = NA, Livro = NA, Ed = NA,
-         Local = NA, Editora = NA, ISBN = NA, pInicial = NA, pFinal = NA)
+      capdat <- data.frame(Ano = NA, Titulo = NA, Livro = NA, Editora = NA, ISBN = NA)
    }
 
    # orientacoes -------------------------------------------------------------
    orien <- try(as_list(xml_children(XML))[[grep("ORIENTACOES-CONCLUIDAS", xml_children(XML))]][["ORIENTACOES-CONCLUIDAS"]], silent = TRUE)
    if (class(orien) != "try-error" & !is.null(orien)) {
       # loop
-      MS = DS = Outra = factor(levels = seq(anos[1], anos[2]))
+      MS = DS = Outra = c()
       for(i in grep("MESTRADO", orien)) {
-      if(is.null(attr(orien[[i]][["DADOS-BASICOS-DE-ORIENTACOES-CONCLUIDAS-PARA-MESTRADO"]], "ANO")))
-         MS[i] <- NA else
          MS[i] <- attr(orien[[i]][["DADOS-BASICOS-DE-ORIENTACOES-CONCLUIDAS-PARA-MESTRADO"]], "ANO")
       }
       for(i in grep("DOUTORADO", orien)) {
-         if(is.null(attr(orien[[i]][["DADOS-BASICOS-DE-ORIENTACOES-CONCLUIDAS-PARA-DOUTORADO"]], "ANO")))
-            DS[i] <- NA else
-            DS[i] <- attr(orien[[i]][["DADOS-BASICOS-DE-ORIENTACOES-CONCLUIDAS-PARA-DOUTORADO"]], "ANO")
+         DS[i] <- attr(orien[[i]][["DADOS-BASICOS-DE-ORIENTACOES-CONCLUIDAS-PARA-DOUTORADO"]], "ANO")
       }
       for(i in grep("OUTRAS", orien)) {
-         if(is.null(attr(orien[[i]][["DADOS-BASICOS-DE-OUTRAS-ORIENTACOES-CONCLUIDAS"]], "ANO")))
-            Outra[i] <- NA else
-            Outra[i] <- attr(orien[[i]][["DADOS-BASICOS-DE-OUTRAS-ORIENTACOES-CONCLUIDAS"]], "ANO")
+         Outra[i] <- attr(orien[[i]][["DADOS-BASICOS-DE-OUTRAS-ORIENTACOES-CONCLUIDAS"]], "ANO")
       }
-      outorien <- c(Doutorado = sum(table(DS)), Mestrado = sum(table(MS)), 
-         Outras_orient = sum(table(Outra)))
+      outorien <- c(Mestrado = sum(as.numeric(MS) >= anos[1] & as.numeric(MS) <= anos[2], na.rm=TRUE), 
+         Doutorado = sum(as.numeric(DS) >= anos[1] & as.numeric(DS) <= anos[2], na.rm=TRUE),
+         Outras_orient = sum(as.numeric(Outra) >= anos[1] & as.numeric(Outra) <= anos[2], na.rm=TRUE))
    } else { 
       outorien = c(Mestrado = 0, Doutorado = 0, Outras_orient = 0)
    }
@@ -271,11 +177,34 @@ artigosLattes <- function(XML, anos)
    }
    patdat$Registro <- factor(patdat$Registro, levels = c(FALSE, TRUE))
 
+   # artigos em revistas ou jornais -------------------------------------------
+   artt <- try(as_list(xml_children(prod)[[grep("TEXTOS-EM-JORNAIS-OU-REVISTAS", xml_children(prod))]]), silent = TRUE)
+   if (class(artt) != "try-error" & !is.null(artt)) {
+      arttdat <- data.frame(Ano = as.numeric(sapply(lapply(artt, "[[", "DADOS-BASICOS-DO-TEXTO"), attr, "ANO-DO-TEXTO")), 
+         Titulo = NA, Revista = NA)
+   } else { 
+      arttdat = data.frame(Ano = NA, Titulo = NA, Revista = NA)  
+   }
+
+   # org evento --------------------------------------------------------------
+   t1 <- try(xml_children(XML)[[grep("PRODUCAO-TECNICA", xml_children(XML))]], silent = TRUE)
+   t2 <- try(xml_children(t1)[[grep("DEMAIS-TIPOS-DE-PRODUCAO-TECNICA>", xml_children(t1))]], silent = TRUE)
+   org <- try(as_list(xml_children(t2)[grep("ORGANIZACAO-DE-EVENTO", xml_children(t2))]), silent = TRUE)
+   if (class(org) != "try-error" & !is.null(org)) {
+      orgdat <- data.frame(Ano = as.numeric(sapply(lapply(org, "[[", "DADOS-BASICOS-DA-ORGANIZACAO-DE-EVENTO"), attr, "ANO")), 
+         Titulo = sapply(lapply(org, "[[", "DADOS-BASICOS-DA-ORGANIZACAO-DE-EVENTO"), attr, "TITULO"))
+   } else { 
+      orgdat = data.frame(Ano = NA, Titulo = NA)  
+   }
+
    # output ------------------------------------------------------------------
    artigodat <- subset(artigodat, Ano >= anos[1] & Ano <= anos[2])
    metrics <- table(artigodat$Qualis)
    resumo <- c(metrics, Total_art = sum(metrics, na.rm=TRUE), 
       Qualis_A = sum(metrics[1:4], na.rm=TRUE))
+
+   arttdat <- subset(arttdat, Ano >= anos[1] & Ano <= anos[2])
+   outartt <- c(Artigos_tec = nrow(arttdat))
 
    trabdat <- subset(trabdat, Ano >= anos[1] & Ano <= anos[2])
    tabT <- table(trabdat$Natureza)
@@ -291,11 +220,73 @@ artigosLattes <- function(XML, anos)
    patdat <- subset(patdat, Ano >= anos[1] & Ano <= anos[2])
    outpat <- c(Patentes = nrow(patdat))
 
+   orgdat <- subset(orgdat, Ano >= anos[1] & Ano <= anos[2])
+   outorg <- c(Org_eventos = nrow(orgdat))
+
    out <- list(nome = nome, atual = datat., metricas = resumo, 
-      planilha = artigodat, trabalhos = resumoT,
+      artigos_tec = outartt, trabalhos = resumoT,
       livros = outlivro, capitulos = outcap, orientacoes = outorien,
-      software = outsoft, patentes = outpat )
+      software = outsoft, patentes = outpat, org = outorg)
    return(out)
+}
+
+
+# --------------------------------------------------
+# latticles para multiplos curriculos
+
+latticles_mult <- function(direc, anos)
+{
+   if(is.null(direc)) return()
+   if(is.null(anos)) return()
+
+   # arquivos zip no diretorio
+   files <- list.files(direc, pattern = "zip|xml")
+   n <- length(files)
+
+   # loop
+   progress <- shiny::Progress$new()
+   on.exit(progress$close())
+   xmls <- quanti <- list()
+   for(i in 1:n) {
+      xmls[[i]] <- read_xml(paste(direc, "\\", files[i], sep = ""))
+      quanti[[i]] <- artigosLattes(XML = xmls[[i]], anos = anos)
+      progress$set(message = "Computando as producoes", value = i/n,
+         detail = paste("Curriculo", i, "de", n))
+   }
+
+   # artigos pub
+   spam <- do.call("rbind", lapply(quanti, "[[", "metricas"))
+
+   # artigos em revistas ou jornais
+   spamAt <- do.call("rbind", lapply(quanti, "[[", "artigos_tec"))
+
+   # trabalhos e resumos em eventos 
+   spamT <- do.call("rbind", lapply(quanti, "[[", "trabalhos"))
+
+   # livros e capitulos
+   spamL <- do.call("rbind", lapply(quanti, "[[", "livros"))
+   spamC <- do.call("rbind", lapply(quanti, "[[", "capitulos"))
+
+   # orientacoes
+   spamO <- do.call("rbind", lapply(quanti, "[[", "orientacoes"))
+
+   # softwares e patentes
+   spamS <- do.call("rbind", lapply(quanti, "[[", "software"))
+   spamP <- do.call("rbind", lapply(quanti, "[[", "patentes"))
+
+   # org eventos
+   spamOr <- do.call("rbind", lapply(quanti, "[[", "org"))
+
+   # output
+   nomes <- sapply(quanti, "[[", "nome")
+   out <- cbind(artigos = spam, artigos_tec = spamAt, trabalhos = spamT,
+      livros = spamL, capitulos = spamC, orien = spamO, 
+      soft = spamS, pat = spamP, org = spamOr)
+   out2 <- data.frame("Nome-----------------------------------------------------------" = c(nomes, "TOTAL"), 
+      Data = c(sapply(quanti, "[[", "atual"), " "), 
+      rbind(out, TOTAL = colSums(out)))
+   rownames(out2) <- NULL
+   invisible(out2)
 }
 
 
@@ -308,27 +299,23 @@ server <- shinyServer(function(input, output, session){
    output$direc <- renderPrint(direc())
    anos <- reactive({
       ano <- input$range
-      if(is.null(ano)) return() else as.integer(ano)
+      if(is.null(ano)) return(NULL) else as.integer(ano)
    })
    output$producao <- renderTable({ 
-      latticles2(direc(), anos())
+      latticles_mult(direc(), anos())
    }, digits = 0, spacing = "xs")
    output$downloadData <- downloadHandler(
          filename = function() {
             paste("producao", ".csv", sep = "")
          },
          content = function(file) {
-            write.csv(latticles2(direc(), anos()), file)
+            write.csv(latticles_mult(direc(), anos()), file)
    })
    onSessionEnded = function(callback) {
-      "Registers the given callback to be invoked when the session is closed
-      (i.e. the connection to the client has been severed). The return value
-      is a function which unregisters the callback. If multiple callbacks are
-      registered, the order in which they are invoked is not guaranteed."
       return(.closedCallbacks$register(callback))
    }
    session$onSessionEnded(function() {
-        stopApp()
+      stopApp()
    })
 })
 
@@ -352,9 +339,9 @@ ui <- fluidPage(
          h6("Powered by:",
          tags$img(src = "agrometrics.jpg", heigth = 110, width = 110),
          tags$img(src = "logoIF.png", heigth = 90, width = 90),
-         helpText("Desenvolvido por: da Silva, A. R. (C) Copyright 2020\n
+         helpText("Desenvolvido por: Da Silva, A. R. (C) Copyright 2020\n
                   <anderson.silva[at]ifgoiano.edu.br>"),
-         helpText("da Silva, A.R. (2019). Latticles: avaliacao eficiente da 
+         helpText("Da Silva, A.R. (2019). Latticles: avaliacao eficiente da 
                   producao cientifica. Programa de computador. 
                   Registro INPI: BR512019001166-0. <https://arsilva87.github.io/latticles_app/>"),
 	 helpText("GNU General Public Licence, version 3.0")
@@ -368,12 +355,14 @@ ui <- fluidPage(
               NP: artigos publicados em nao periodicos, 
               Total_art: total de artigos no periodo,
               Qualis_A: total de artigos A1-A4, 
-              COMPLETO: trabalhos completos em anais de eventos,
-              RESUMO/RESUMO_EXPANDIDO: resumos/resumos expandidos em eventos, 
+              Artigos_tec: artigos tecnicos em revistas ou jornais,
+              Completo: trabalhos completos em anais de eventos,
+              Res_simples/Res_expandido: resumos em anais de eventos, 
               Livros/Capitulos: livros/capitulos publicados no periodo,
               Doutorado/Mestrado/Outras_orient: orientacoes concluidas no periodo,
               Software_nReg/Reg: programas de computador nao/registrados,
-              Patentes: patentes concedidas.")
+              Patentes: patentes concedidas,
+              Org_eventos: eventos tecnico-cientificos organizados.")
          )
       )
    )
@@ -381,4 +370,3 @@ ui <- fluidPage(
 
 # Run the app
 shinyApp(ui = ui, server = server)
-
